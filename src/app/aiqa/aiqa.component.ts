3ea05dc12240d82498f7514fc5e665fba37f2ab4
jennifershih <s01059810@gmail.com>
2023-03-10 10:31:37 +0800

    Feat: modify aiqa logic

--- src/app/aiqa/aiqa.component.ts
+++ src/app/aiqa/aiqa.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface Completion {
-  choices: {
-    text: string;
-    index: number;
-    logprobs: any;
-    finish_reason: string;
-  }[];
+interface BibleVerseResponse {
+  verse: string;
 }
 
 @Component({
@@ -27,33 +22,15 @@ export class AiqaComponent {
   }
 
   async submit() {
-    const prompt = `今天我的心情是 ${this.mood}，可以幫我找一個聖經經文給我嗎？請務必確保聖經經文是正確的。\n`;
-    const apiKey = this.apiKey; // 從input中讀取apiKey的值
-    const res = await this.createCompletion(prompt, apiKey);
-
-    if (res && res.choices && res.choices.length > 0) {
-      const bibleVerse = res.choices[0].text;
-      this.bibleVerse = bibleVerse;
+    const res = await this.createCompletion(this.mood);
+    if (res && res.verse) {
+      this.bibleVerse = res.verse;
+      console.log(this.bibleVerse);
     }
   }
-
-  private async createCompletion(prompt: string, apiKey: string) {
-    const endpoint = `https://api.openai.com/v1/completions`;
-    const body = {
-      model: 'text-davinci-003',
-      prompt: prompt,
-      max_tokens: 700,
-      temperature: 0.7,
-      stream: false,
-    };
-
-    return this.http
-      .post<Completion>(endpoint, body, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${apiKey}`,
-        },
-      })
-      .toPromise();
+  private async createCompletion(mood: string) {
+    const endpoint = `http://localhost:3000/get-bible-verse-from-mood`;
+    const body = { mood: mood };
+    return this.http.post<BibleVerseResponse>(endpoint, body).toPromise();
   }
 }
